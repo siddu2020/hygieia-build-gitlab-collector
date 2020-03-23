322ae6084e930b8ff00fcec79c7b5021e45778d1
thw7696 <hanishap@thoughtworks.com>
2020-03-23 17:46:31 +0530

    Fix residual Commits in Build Stage

--- src/main/java/com/capitalone/dashboard/collector/DefaultGitlabClient.java
+++ src/main/java/com/capitalone/dashboard/collector/DefaultGitlabClient.java
@@ -335,6 +335,7 @@ public class DefaultGitlabClient implements GitlabClient {
         List<String> dashBoardIds = allDashboardsForCommit.stream().map(d -> d.getId().toString()).collect(Collectors.toList());
 
         String environmentName = PipelineStage.BUILD.getName();
+        String commitStageName = PipelineStage.COMMIT.getName();
         List<Collector> collectorList = collectorRepository.findByCollectorType(CollectorType.Product);
         List<CollectorItem> collectorItemList = collectorItemRepository.findByCollectorIdIn(collectorList.stream().map(BaseModel::getId).collect(Collectors.toList()));
 
@@ -352,12 +353,24 @@ public class DefaultGitlabClient implements GitlabClient {
                 if (environmentStage.getCommits() == null) {
                     environmentStage.setCommits(new HashSet<>());
                 }
+
+                HashSet<PipelineCommit> pipelineCommits = new HashSet<>();
+                EnvironmentStage commitStage = environmentStageMap.get(commitStageName);
+
+                if(commitStage != null && commits.size() > 0) {
+                    for (Commit commit : commits) {
+                        List<PipelineCommit> cs = commitStage.getCommits().stream()
+                                .filter(c -> c.getScmParentRevisionNumbers().size() > 1 && c.getTimestamp() < commit.getTimestamp()).collect(Collectors.toList());
+                        pipelineCommits.addAll(cs);
+                    }
+                }
                 environmentStage.getCommits().addAll(commits.stream()
                         .map(commit -> {
                             //commit.setTimestamp(timestamp); <--- Not sure if we should do this.
                             // IMO, the SCM timestamp should be unaltered.
                             return new PipelineCommit(commit, timestamp);
                         }).collect(Collectors.toSet()));
+                environmentStage.getCommits().addAll(pipelineCommits);
                 pipelineRepository.save(pipeline);
             }
         }
